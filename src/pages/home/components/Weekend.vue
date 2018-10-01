f930e3b5df44fd27bd18d418187fc5e61d334ea7
278097089@qq.com <you@example.com>
2018-10-01 16:07:06 +0800

    weekend

---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,68 @@
+<template>
+  <div>
+    <div class="title">周末游推荐</div>
+    <ul>
+      <li class="border-bottom" v-for="item of weekendList" :key="item.id">
+        <div class="wrapper">
+          <img class="wrapper-img" :src="item.imgUrl" />
+        </div>
+        <div class="content">
+          <p class="content-title">{{item.title}}</p>
+          <span class="content-desc">{{item.desc}}</span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      weekendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/9f/f585152825459.jpg_r_640x214_5d46e4cc.jpg',
+        title: '大连圣亚海洋世界',
+        desc: '浪漫大连首站，浪漫的海洋主题乐园'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
+  .title
+    margin-top: .2rem
+    line-height: .8rem
+    background: #eee
+    text-indent: .2rem
+  .wrapper
+    overflow: hidden
+    height: 0
+    padding-bottom: 31.24%
+    .wrapper-img
+      width: 100%
+  .content
+    padding: .1rem
+    text-align: center
+    .content-title
+      line-hieght: .54rem
+      font-size: .32rem
+      ellipsis()
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+      ellipsis()
+</style>
