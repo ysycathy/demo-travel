49e67848042443d06de37f893a4efb0d97571c17
278097089@qq.com <you@example.com>
2018-10-02 12:35:11 +0800

    search-input

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -1,17 +1,23 @@
 <template>
-    <div>
-      <div class="title">热销推荐</div>
-      <ul>
-        <li class="item border-bottom" v-for="item of list" :key="item.id">
-          <img :src="item.imgUrl" />
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <router-link
+          tag="li"
+          class="item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          :to="'/detail/' + item.id"
+        >
+          <img class="item-img" :src="item.imgUrl" />
           <div class="item-info">
             <p class="item-title">{{item.title}}</p>
             <p class="item-desc">{{item.desc}}</p>
             <button class="item-button">查看详情</button>
           </div>
-        </li>
-      </ul>
-    </div>
+        </router-link>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -24,30 +30,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  @import '~styles/mixins.styl'
   .title
     margin-top: .2rem
     line-height: .8rem
     background: #eee
     text-indent: .2rem
+    text-align: left
   .item
     overflow: hidden
     display: flex
     height: 1.9rem
     .item-img
-      width: 1.7rem
+      width: 2.7rem
       height: 1.7rem
-      padding: .1rem
+      padding: .2rem
     .item-info
       flex: 1
-      padding: .1rem
+      padding-top: .1rem
       min-width: 0
+      text-align left
+      margin-left .1rem
       .item-title
         line-height: .54rem
         font-size: .32rem
         ellipsis()
       .item-desc
         line-height: .4rem
-        color: #b6b6b6
+        color: #333333ba
         ellipsis()
       .item-button
         line-height: .44rem
